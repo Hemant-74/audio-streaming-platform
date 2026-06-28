f301c725eff28811f85f9256f0d9e600542e0adf
Hemant Agarwal <hardikagrawal742700@gmail.com>
2026-06-28 13:49:37 +0530

    Fix: Added Vite base URL and optimized Redux in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,28 @@
 import { FaPlay } from "react-icons/fa";
-import { setBrowsingSongs , setActiveTab } from "../store/songSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { setBrowsingSongs, setActiveTab } from "../store/songSlice";
+import { useDispatch } from "react-redux";
 const Card = ({ album }) => {
-
-  const { browsingSongs , activeTab } = useSelector((state) => state.song);
   const dispatch = useDispatch();
 
   const handleAlbumClick = () => {
     dispatch(setBrowsingSongs(album.songs));
     dispatch(setActiveTab("browsing"));
-  }
+  };
 
   return (
-    <div onClick={handleAlbumClick} data-folder={album.folderName} className="card">
+    <div
+      onClick={handleAlbumClick}
+      data-folder={album.folderName}
+      className="card"
+    >
       <div className="outer_div">
-      <div className="play">
-        <FaPlay color="black"/>
-      </div>
-      <img
-        src={`${import.meta.env.BASE_URL}s/${album.folderName}/cover.jpg`}
-        alt=""
-      />
+        <div className="play">
+          <FaPlay color="black" />
+        </div>
+        <img
+          src={`${import.meta.env.BASE_URL}s/${album.folderName}/cover.jpg`}
+          alt={album.title}
+        />
       </div>
       <h2>{album.title}</h2>
       <p>{album.description}</p>
